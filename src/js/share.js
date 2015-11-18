36f56b650944cef6501db96b17819e003782da92
Jake Champion <jakechampion.jake2@googlemail.com>
2015-11-18 16:16:04 +0000

    use live service

--- src/js/share.js
+++ src/js/share.js
@@ -1,5 +1,5 @@
 	/**global require, module, window, CustomEvent, document, HTMLElement, location */
-	const serviceURL = "https://sharecode-test.ft.com";
+	const serviceURL = "https://sharecode.ft.com";
 	const DomDelegate = require('ftdomdelegate');
 	const qs = require('query-string');
 	const Tooltip = require('./Tooltip');
